8094fd493a0327c7e5093ef62bcc20e859df68d7
OCC <occ@occ-R560.(none)>
2013-08-23 18:24:16 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EleganBlog::Application.config.secret_key_base = 'fd88568b74c598eb27c215007ab5bcd1ce91c522355f49a58e9e9cfc863e9f4558507da69343268fbfcf60a020ac1bd849772e3d9234e4bc57d343dc6dec6336'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EleganBlog::Application.config.session_store :cookie_store, key: '_elegan_blog_session'
